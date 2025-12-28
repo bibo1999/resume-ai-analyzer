7a31bed785133875069a365bc1ad74ef56ba3d86
Abdelrahman <abdelrahmanmoataz19@gmail.com>
2025-12-29 01:40:31 +0200

    Resume Feedback page

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -60,7 +60,7 @@ const Upload = () => {
         await kv.set(`resume:${uuid}`, JSON.stringify(data));
         setStatusText('Analysis complete, redirecting...');
         console.log(data);
-        // navigate(`/resume/${uuid}`); commented till I build the resume page
+        navigate(`/resume/${uuid}`);
     }
 
     const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
