2658be49dd689252b92d759e882b2acf68689313
Abdelrahman <abdelrahmanmoataz19@gmail.com>
2026-01-03 21:59:59 +0200

    Displaying the resume submition on the homepage

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -1,8 +1,25 @@
 import { Link } from "react-router"
 import ScoreCircle from "./ScoreCircle"
+import { usePuterStore } from "~/lib/puter"
+import { useEffect, useState } from "react"
 
 const ResumeCard = ({resume: { id, companyName, jobTitle, feedback, imagePath} }: {resume: Resume}) => {
-  return (
+    
+    const { fs } = usePuterStore();
+    const [resumeUrl, setResumeUrl] = useState('');
+
+    useEffect(() => {
+        const loadResume = async () => {
+            const blob = await fs.read(imagePath);
+            if(!blob) return;
+            let url = URL.createObjectURL(blob);
+            setResumeUrl(url);
+        }
+
+        loadResume();
+    }, [imagePath]);
+
+    return (
     <Link to={`/resume/${id}`} className="resume-card animate-in fade-in duration-1000">
         <div className="resume-card-header">
         <div className="flex flex-col gap-2">
@@ -17,16 +34,17 @@ const ResumeCard = ({resume: { id, companyName, jobTitle, feedback, imagePath} }
             <ScoreCircle score={feedback.overallScore}/>
         </div>
         </div>
+        {resumeUrl && (
         <div className="gradient-border animate-in fade-in duration-1000">
             <div className="w-full h-full">
                 <img
-                    src={imagePath}
+                    src={resumeUrl}
                     alt="resume"
                     className="w-full h-[350px] max-sm:h-[200px] object-cover object-top"
                 />
             </div>
-
         </div>
+        )}
     </Link>
   )
 }
